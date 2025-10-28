22ddccfed2b2d74ac9a019cad7faa7f5c72eba12
Prerit Siwach <preri@MacBook-Air.local>
2025-10-28 14:18:04 +0530

    27-10-2025

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const app = express();
+
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Meow Meow');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
